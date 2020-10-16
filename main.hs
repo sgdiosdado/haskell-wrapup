f1d81115d8c0bf51058b0931aea99b2d2d342ae0
Sergio Diosdado <sgdc_98@hotmail.com>
2020-10-16 09:03:34 -0500

    Add exercises 4 and 8

--- main.hs
+++ main.hs
@@ -1,3 +1,10 @@
+-- Equipo:
+-- * Sergio Diosdado      A00516971
+-- * Iñaki Janeiro        A00516978
+-- * Eduardo Guzmán Vega  A01194108
+
+import Data.List
+
 -- ====================
 --      UTILIDADES  
 -- ==================== 
@@ -21,14 +28,6 @@ es_primo n
 data AB t = 
   A (AB t) t (AB t)
   | V deriving Show
-
--- sub arbol izquierdo
-izq :: AB a -> AB a
-izq (A l n r) = l
-
--- sub arbol derecho
-der :: AB a -> AB a
-der (A l n r) = r 
 
 -- Arbol de ejemplo
 ab = 
@@ -70,11 +69,13 @@ mayores (x:xs) (y:ys)
   | y <= x = 1 : mayores xs ys
   | otherwise = 2 : mayores xs ys
 
--- multiplica
--- multiplica :: [Int] -> [Int] -> [Int]
--- multiplica [] y = []
--- multiplica x [] = []
--- multiplica x y  = 
+-- Funcion multiplica
+multiplica_aux :: [Int] -> [Int] -> [Int] -> [Int]
+multiplica_aux [] _ lista_acum = lista_acum
+multiplica_aux lista1 lista2 lista_acum = lista_acum ++ lista2 ++ multiplica_aux (tail lista1) lista2 lista_acum
+
+multiplica :: [Int] -> [Int] -> [Int]
+multiplica lista1 lista2 = multiplica_aux lista1 lista2 []
 
 -- desplaza
 desplaza :: [Int] -> Int -> [Int]
@@ -90,6 +91,13 @@ impares (A l n r)
   | otherwise = (impares l) ++ (impares r)
 
 -- g_distintos
+g_distintos :: (Eq a) => [a] -> [a] -> [a]
+g_distintos a [] = a
+g_distintos [] b = b
+g_distintos (a:as) (b:bs)
+  | notElem a (b:bs) = [a] ++ g_distintos as (delete a (b:bs))
+  | notElem b (a:as) = [b] ++ g_distintos bs (delete b (a:as))
+  | otherwise = [] ++ g_distintos (delete a bs) (delete b as)
 
 -- c_tabla
 c_tabla n = [((n, x), (x*n)) | x <- [1..10]]
@@ -98,23 +106,27 @@ c_tabla n = [((n, x), (x*n)) | x <- [1..10]]
 f_prodpar l = map (foldl (*) 1) (filter (odd . length) l)
 
 main = do
-  -- print(maxi 1 2)
-  -- print(mini 1 2)
-  -- print(medio 2 1 5 4)
-  -- print(medio 2 2 2 2)
-  -- print(primos 1 10)
-  -- print(primos 5 11)
-  -- print(primos 8 10)
-  -- print(mayores [8,5,2,4] [1,2,3,4])
-  -- print(mayores [1,2,3] [2,3,1])
-  -- print(multiplica[1,1,1] [])
-  -- print(desplaza [1,2,3] 1)
-  -- print(desplaza [1,2,3] 2)
-  -- print(desplaza [1,2,3] 6)
-  -- print(impares V)
-  -- print(impares ab)
-  -- print(impares (A (A (A V 3 V) 2 V) 1 (A (A V 5 V) 4 V)))
-  -- print(c_tabla 1)
-  -- print(c_tabla 8)
+  print(maxi 1 2)
+  print(mini 1 2)
+  print(medio 2 1 5 4)
+  print(medio 2 2 2 2)
+  print(primos 1 10)
+  print(primos 5 11)
+  print(primos 8 10)
+  print(mayores [8,5,2,4] [1,2,3,4])
+  print(mayores [1,2,3] [2,3,1])
+  print(multiplica [1,1] [1,1,1])
+  print(multiplica[1,1,1] [])
+  print(multiplica [1,1,1,1] [1,1])
+  print(desplaza [1,2,3] 1)
+  print(desplaza [1,2,3] 2)
+  print(desplaza [1,2,3] 6)
+  print(impares V)
+  print(impares ab)
+  print(impares (A (A (A V 3 V) 2 V) 1 (A (A V 5 V) 4 V)))
+  print(c_tabla 1)
+  print(c_tabla 8)
   print(f_prodpar [[1,2,3],[4,5],[6,7]])
   print(f_prodpar [[1],[1,2],[1,2,3],[4,3,2]])
+  print(g_distintos ['a','b','c'] ['d','f','a'])
+  print(g_distintos [1,2,3] [2,3,1])
